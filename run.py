73809e04bb2591be88c20f7a4033c20decb8abae
rcolin <remi.collin@gmail.com>
2016-07-17 20:17:59 +0200

    add main method and def

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
+import os
 
+
+def testfct():
+    print('number of pool : ');
+    nPool = input();
+    print('number of iterations : ');
+    nIterations = input();
+    return (nPool, nIterations);
 
 if __name__ == '__main__':
-    print 'hello wordl'
+   os.system('clear');
+   print(' ************** Genetic algorithm of beetle moving **************');
+   nPool, nIterations = testfct();
+   print ' #Pool : ' + str(nPool) + ' | #Iterations : ' + str(nIterations);
