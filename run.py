8717ff6f6301a62834ab17bc73bb77b7c11058b9
rcolin <remi.collin@gmail.com>
2016-07-17 22:05:36 +0200

    add parse args method

--- run.py
+++ run.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python
 
 import os
+import sys, getopt
 from beetle import Beetle
 
-def inputdata():
-    print('number of pool : ');
-    nPool = input();
-    print('number of iterations : ');
-    nIterations = input();
-    return (nPool, nIterations);
+def inputdata(argv):
+    poolsize = '0';
+    iterations = '0';
+    
+    try:
+      opts, args = getopt.getopt(argv,"hp:i:",["--poolsize","--iterations"])
+    except getopt.GetoptError as err:
+      print 'run.py -p <poolsize> -i <iterations>'
+      sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'run.py -p <poolsize> -i <iterations>'
+            sys.exit()
+        elif opt in ("-p", "--poolsize"):
+            poolsize = arg
+        elif opt in ("-i", "--iterations"):
+            iterations = arg
+        else:
+            assert False, "unhandled option"
+    return (poolsize, iterations)
 
-if __name__ == '__main__':
+def intro():
     os.system('clear');
     print('\n************** Genetic algorithm of beetle moving **************\n');
+
+if __name__ == '__main__':
+    #display start infos
+    intro();
     
-    nPool, nIterations = inputdata();
+    #input data
+    poolsize = 0
+    iterations = 0
+    poolsize, iterations = inputdata(sys.argv[1:]);
     
-    print ' #Pool : ' + str(nPool) + ' | #Iterations : ' + str(nIterations);
+    #display input data
+    print ' #Pool : ' + poolsize + ' | #Iterations : ' + iterations;
    
-    for x in range(0, nPool):
-        b = Beetle("b" + str(x));
+    #creat beetle pool
+    for i in range(0, int(poolsize)):
+        b = Beetle("b" + str(i));
         print(b.name);
-        
