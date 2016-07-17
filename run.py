ed42f6452d8e311b8d611f79546f8cdc1769c919
rcolin <remi.collin@gmail.com>
2016-07-17 20:54:27 +0200

    rename inputdata

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+
 import os
+from beetle import Beetle
 
-
-def testfct():
+def inputdata():
     print('number of pool : ');
     nPool = input();
     print('number of iterations : ');
@@ -9,7 +11,14 @@ def testfct():
     return (nPool, nIterations);
 
 if __name__ == '__main__':
-   os.system('clear');
-   print(' ************** Genetic algorithm of beetle moving **************');
-   nPool, nIterations = testfct();
-   print ' #Pool : ' + str(nPool) + ' | #Iterations : ' + str(nIterations);
+    os.system('clear');
+    print('\n************** Genetic algorithm of beetle moving **************\n');
+    
+    nPool, nIterations = inputdata();
+    
+    print ' #Pool : ' + str(nPool) + ' | #Iterations : ' + str(nIterations);
+   
+    for x in range(0, nPool):
+        b = Beetle("b" + str(x));
+        print(b.name);
+        
